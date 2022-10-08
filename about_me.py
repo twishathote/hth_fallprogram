f9f85458428dcce4728b71bb4500e04f003f99de
twishathote <115053363+twishathote@users.noreply.github.com>
2022-10-08 10:24:33 -0700

    Add files via upload

--- about_me.py
+++ about_me.py
@@ -1,1 +1,10 @@
-print("hello")
+str = "Twisha Thote"
+nickname = "Wish"
+age = int(15)
+used_python = True
+hobbies = ["reading", "coding", "listening to music", "singing"]
+favorites_dictionary = {
+  "movie": "Inception", 
+  "band": "5SOS",
+  "food": "cake"}
+all_vars = dict(vars())
